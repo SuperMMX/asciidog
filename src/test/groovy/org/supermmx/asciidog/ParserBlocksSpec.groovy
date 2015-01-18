c47689379050800e91f62a3e41d494afa3bccec5
Xuan MA <SuperMMX@gmail.com>
2015-01-18 21:52:04 +1100

    Add spec for blocks parsing

---
+++ src/test/groovy/org/supermmx/asciidog/ParserBlocksSpec.groovy
@@ -0,0 +1,76 @@
+package org.supermmx.asciidog
+
+import org.supermmx.asciidog.ast.Author
+import org.supermmx.asciidog.ast.Block
+import org.supermmx.asciidog.ast.Header
+import org.supermmx.asciidog.ast.Paragraph
+
+import spock.lang.*
+
+class ParserBlocksSpec extends Specification {
+    def 'parse: blocks: single block'() {
+        given:
+        def content = 'paragraph'
+
+        def parser = new Parser()
+        def reader = Reader.createFromString(content)
+        parser.reader = reader
+
+        when:
+        def blocks = parser.parseBlocks(new Block())
+
+        then:
+        blocks.size() == 1
+        blocks[0].lines == [ 'paragraph' ]
+    }
+
+    def 'parse: blocks: multiple blocks leading blank lines'() {
+        given:
+        def content = '''
+
+new paragraph
+with another line
+
+second paragraph
+
+'''
+
+        def parser = new Parser()
+        def reader = Reader.createFromString(content)
+        parser.reader = reader
+
+        when:
+        def lines = parser.parseBlocks(new Block())
+
+        then:
+        lines.size() == 2
+        lines[0].lines == [ 'new paragraph', 'with another line' ]
+        lines[1].lines == [ 'second paragraph' ]
+    }
+
+    def 'parse: blocks: multiple blocks with section after '() {
+        given:
+        def content = '''
+
+new paragraph
+with another line
+
+second paragraph
+
+== Section Title
+
+'''
+
+        def parser = new Parser()
+        def reader = Reader.createFromString(content)
+        parser.reader = reader
+
+        when:
+        def lines = parser.parseBlocks(new Block())
+
+        then:
+        lines.size() == 2
+        lines[0].lines == [ 'new paragraph', 'with another line' ]
+        lines[1].lines == [ 'second paragraph' ]
+    }
+}
