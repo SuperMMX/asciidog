af4784bf4f50a12600b8cbbdbad49aecec99f364
Xuan MA <SuperMMX@gmail.com>
2015-02-21 22:28:07 +1100

    Add error checking for invalid section levels for article

--- src/test/groovy/org/supermmx/asciidog/ParserDocumentSpec.groovy
+++ src/test/groovy/org/supermmx/asciidog/ParserDocumentSpec.groovy
@@ -7,6 +7,14 @@ import org.supermmx.asciidog.ast.Header
 import spock.lang.*
 
 class ParserDocumentSpec extends Specification {
+    @Shared
+    def builder = new ObjectGraphBuilder()
+
+    def setupSpec() {
+        builder.classNameResolver = "org.supermmx.asciidog.ast"
+        builder.identifierResolver = "uid"
+    }
+
     def 'parse: document: doctype'() {
         given:
         def content = '''
@@ -48,5 +56,25 @@ and a new paragraph
         doc.blocks.size() == 2
         doc.blocks[0].lines == [ 'this is the paragraph', 'in the preamble', 'of the document' ]
         doc.blocks[1].lines == [ 'and a new paragraph' ]
+    }
+
+    def 'wrong section level in article'() {
+        given:
+        def content = '''
+= Document Title
+
+= Section
+'''
+        def parser = new Parser()
+        def reader = Reader.createFromString(content)
+        parser.reader = reader
+
+        when:
+        def doc = parser.parseDocument()
+
+        then:
+        doc == builder.document(docType: Document.DocType.article) {
+            header(title: 'Document Title')
+        }
     }
 }
