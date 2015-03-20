3b88f24ad7b7d893527796a84e2a46046899bf28
Xuan MA <SuperMMX@gmail.com>
2015-03-20 18:32:18 +1100

    Add constrained and escaped in inline info

--- src/main/groovy/org/supermmx/asciidog/ast/InlineInfo.groovy
+++ src/main/groovy/org/supermmx/asciidog/ast/InlineInfo.groovy
@@ -4,6 +4,9 @@ package org.supermmx.asciidog.ast
  * An inline container that contains multiple inline nodes.
  */
 trait InlineInfo {
+    boolean constrained
+
+    boolean escaped
     int start
     int end
     int contentStart
