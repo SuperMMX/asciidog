7805ab3ba83a9f2c07c1230ffe9f8934a0899db1
Xuan MA <SuperMMX@gmail.com>
2015-03-20 21:52:06 +1100

    Specify node type for plugin

--- src/main/groovy/org/supermmx/asciidog/plugin/TextFormattingInlineParserPlugin.groovy
+++ src/main/groovy/org/supermmx/asciidog/plugin/TextFormattingInlineParserPlugin.groovy
@@ -1,12 +1,17 @@
 package org.supermmx.asciidog.plugin
 
-import org.supermmx.asciidog.ast.Inline
 import org.supermmx.asciidog.ast.FormattingNode
+import org.supermmx.asciidog.ast.Inline
+import org.supermmx.asciidog.ast.Node
 
 import java.util.regex.Matcher
 
 class TextFormattingInlineParserPlugin extends InlineParserPlugin {
     FormattingNode.Type formattingType
+
+    TextFormattingInlineParserPlugin() {
+        nodeType = Node.Type.INLINE_FORMATTED_TEXT
+    }
 
     Inline parse(Matcher m, List<String> groups) {
         FormattingNode inline = new FormattingNode()
