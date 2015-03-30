8a370c3ac4ec61d9346cbf03c189341f9a59289a
Xuan MA <SuperMMX@gmail.com>
2015-03-30 19:09:01 +1100

    Fix paragraph spec

--- src/test/groovy/org/supermmx/asciidog/ParserParagraphSpec.groovy
+++ src/test/groovy/org/supermmx/asciidog/ParserParagraphSpec.groovy
@@ -7,7 +7,7 @@ import org.supermmx.asciidog.ast.Paragraph
 
 import spock.lang.*
 
-class ParserParagraphSpec extends Specification {
+class ParserParagraphSpec extends AsciidogSpec {
     def 'parse: paragraph: single line'() {
         given:
         def content = 'paragraph'
@@ -16,133 +16,111 @@ class ParserParagraphSpec extends Specification {
         def reader = Reader.createFromString(content)
         parser.reader = reader
 
-        when:
-        def para = parser.parseParagraph(new Block())
-
-        then:
-        para != null
-        para.lines == [ 'paragraph' ]
+        expect:
+        parser.parseParagraph(new Block()) == para(content)
     }
 
     def 'parse: paragraph: single line starting with leading blank lines'() {
         given:
-        def content = '''
+        def text = 'paragraph'
+        def content = """
 
 
-paragraph'''
+$text"""
 
         def parser = new Parser()
         def reader = Reader.createFromString(content)
         parser.reader = reader
 
-        when:
-        def para = parser.parseParagraph(new Block())
-
-        then:
-        para != null
-        para.lines == [ 'paragraph' ]
+        expect:
+        parser.parseParagraph(new Block()) == para(text)
     }
 
     def 'parse: paragraph: multiple lines starting with leading blank lines'() {
         given:
-        def content = '''
-
-
-line1
+        def text = '''line1
 line2
 line3'''
+        def content = """
+
+
+$text"""
 
         def parser = new Parser()
         def reader = Reader.createFromString(content)
         parser.reader = reader
 
-        when:
-        def para = parser.parseParagraph(new Block())
-
-        then:
-        para != null
-        para.lines == [ 'line1', 'line2', 'line3' ]
+        expect:
+        parser.parseParagraph(new Block()) == para(text)
     }
 
     def 'parse: paragraph: no new line in the end'() {
         given:
-        def content = '''
-line1
+        def text = '''line1
 line2
 line3'''
+        def content = """
+$text"""
 
         def parser = new Parser()
         def reader = Reader.createFromString(content)
         parser.reader = reader
 
-        when:
-        def para = parser.parseParagraph(new Block())
-
-        then:
-        para != null
-        para.lines == [ 'line1', 'line2', 'line3' ]
+        expect:
+        parser.parseParagraph(new Block()) == para(text)
     }
 
     def 'parse: paragraph: new line in the end'() {
         given:
-        def content = '''
-line1
+        def text = '''line1
 line2
-line3
-'''
+line3'''
+        def content = """
+$text
+"""
 
         def parser = new Parser()
         def reader = Reader.createFromString(content)
         parser.reader = reader
 
-        when:
-        def para = parser.parseParagraph(new Block())
-
-        then:
-        para != null
-        para.lines == [ 'line1', 'line2', 'line3' ]
+        expect:
+        parser.parseParagraph(new Block()) == para(text)
     }
 
     def 'parse: paragraph: blank line after'() {
         given:
-        def content = '''
-line1
+        def text = '''line1
 line2
-line3
+line3'''
+        def content = """
+$text
 
-'''
+"""
 
         def parser = new Parser()
         def reader = Reader.createFromString(content)
         parser.reader = reader
 
-        when:
-        def para = parser.parseParagraph(new Block())
-
-        then:
-        para != null
-        para.lines == [ 'line1', 'line2', 'line3' ]
+        expect:
+        parser.parseParagraph(new Block()) == para(text)
     }
 
     def 'parse: paragraph: another paragraph after'() {
         given:
-        def content = '''
-line1
+        def text = '''line1
 line2
-line3
+line3'''
+        def content = """
+$text
 
 abc
-'''
+"""
 
         def parser = new Parser()
         def reader = Reader.createFromString(content)
         parser.reader = reader
 
-        when:
-        def para = parser.parseParagraph(new Block())
-
-        then:
-        para != null
-        para.lines == [ 'line1', 'line2', 'line3' ]
+        expect:
+        parser.parseParagraph(new Block()) == para(text)
     }
 }
