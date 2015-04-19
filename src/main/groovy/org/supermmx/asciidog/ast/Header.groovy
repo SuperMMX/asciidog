3ae0e0eacc1b774f3bab3933ba54c81291ad3499
Xuan MA <SuperMMX@gmail.com>
2015-04-19 11:02:02 +1000

    Add comments

--- src/main/groovy/org/supermmx/asciidog/ast/Header.groovy
+++ src/main/groovy/org/supermmx/asciidog/ast/Header.groovy
@@ -4,10 +4,12 @@ import groovy.transform.Canonical
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
+/**
+ * Document header.  The children blocks are attributes
+ */
 @Canonical
 @EqualsAndHashCode(callSuper=true)
 @ToString(includeSuper=true, includePackage=false, includeNames=true)
-
 class Header extends Block {
     String title
     List<Author> authors = []
