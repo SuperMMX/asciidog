8660ee6b12222c0cabaef8a9616625d29864c523
Xuan MA <SuperMMX@gmail.com>
2015-06-11 22:50:24 +1000

    Provide new set of methods

--- asciidog/src/main/groovy/org/supermmx/asciidog/backend/AbstractBackend.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/backend/AbstractBackend.groovy
@@ -1,9 +1,64 @@
 package org.supermmx.asciidog.backend
 
+import org.supermmx.asciidog.backend.Backend
+import org.supermmx.asciidog.backend.AbstractBackend
+import org.supermmx.asciidog.ast.AdocList
+import org.supermmx.asciidog.ast.Author
+import org.supermmx.asciidog.ast.Block
+import org.supermmx.asciidog.ast.Document
+import org.supermmx.asciidog.ast.Header
+import org.supermmx.asciidog.ast.Node
+import org.supermmx.asciidog.ast.Section
+import org.supermmx.asciidog.ast.Paragraph
+import org.supermmx.asciidog.ast.Preamble
+import org.supermmx.asciidog.ast.InlineContainer
+import org.supermmx.asciidog.ast.Inline
+import org.supermmx.asciidog.ast.ListItem
+import org.supermmx.asciidog.ast.TextNode
+import org.supermmx.asciidog.ast.FormattingNode
+import org.supermmx.asciidog.ast.CrossReferenceNode
+import org.supermmx.asciidog.converter.DocumentContext
+
 /**
  * Abstract backend
  */
 abstract class AbstractBackend implements Backend {
     String id
     String ext
+
+    void renderDocument(Step step, DocumentContext context, Document document) {
+    }
+
+    void renderDocumentHeader(Step step, DocumentContext context, Document document) {
+    }
+
+    void setAttribute(DocumentContext context, String name, Object value) {
+    }
+
+    void renderPreamble(Step step, DocumentContext context, Preamble preamble) {
+    }
+
+    void renderSection(Step step, DocumentContext context, Section section) {
+    }
+
+    void renderList(Step step, DocumentContext context, AdocList list) {
+    }
+
+    void renderListItem(Step step, DocumentContext context, ListItem item) {
+    }
+
+    void renderParagraph(Step step, DocumentContext context, Paragraph paragraph) {
+    }
+
+    void renderText(DocumentContext context, String text) {
+    }
+
+    void renderInlineText(Step step, DocumentContext context, TextNode textNode) {
+    }
+
+    void renderInlineFormatting(Step step, DocumentContext context, FormattingNode formattingNode) {
+    }
+
+    void renderInlineCrossReference(Step step, DocumentContext context, CrossReferenceNode xrefNode) {
+    }
 }
--- asciidog/src/main/groovy/org/supermmx/asciidog/backend/Backend.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/backend/Backend.groovy
@@ -1,12 +1,31 @@
 package org.supermmx.asciidog.backend
 
+import org.supermmx.asciidog.ast.AdocList
+import org.supermmx.asciidog.ast.Author
+import org.supermmx.asciidog.ast.Block
 import org.supermmx.asciidog.ast.Document
+import org.supermmx.asciidog.ast.Header
+import org.supermmx.asciidog.ast.Node
+import org.supermmx.asciidog.ast.Section
+import org.supermmx.asciidog.ast.Paragraph
+import org.supermmx.asciidog.ast.Preamble
+import org.supermmx.asciidog.ast.InlineContainer
+import org.supermmx.asciidog.ast.Inline
+import org.supermmx.asciidog.ast.ListItem
+import org.supermmx.asciidog.ast.TextNode
+import org.supermmx.asciidog.ast.FormattingNode
+import org.supermmx.asciidog.ast.CrossReferenceNode
+import org.supermmx.asciidog.converter.DocumentContext
 
 /**
  * A backend is responsible for creating the renderer to render
  * the parsed document.
  */
 interface Backend {
+    static enum Step {
+        PRE, CONTENT, POST
+    }
+
     /**
      * The backend id
      */
@@ -18,8 +37,28 @@ interface Backend {
      */
     String getExt()
 
-    /**
-     * Create the renderer with the specified options
-     */
-    Renderer createRenderer(Map<String, Object> options)
+    void renderDocument(Step step, DocumentContext context, Document document)
+
+    void renderDocumentHeader(Step step, DocumentContext context, Document document)
+
+    void setAttribute(DocumentContext context, String name, Object value)
+
+    void renderPreamble(Step step, DocumentContext context, Preamble preamble)
+
+    void renderSection(Step step, DocumentContext context, Section section)
+
+    void renderList(Step step, DocumentContext context, AdocList list)
+
+    void renderListItem(Step step, DocumentContext context, ListItem item)
+
+    void renderParagraph(Step step, DocumentContext context, Paragraph paragraph)
+
+    // Inlines
+    void renderText(DocumentContext context, String text)
+
+    void renderInlineText(Step step, DocumentContext context, TextNode textNode)
+
+    void renderInlineFormatting(Step step, DocumentContext context, FormattingNode formattingNode)
+
+    void renderInlineCrossReference(Step step, DocumentContext context, CrossReferenceNode xrefNode)
 }
