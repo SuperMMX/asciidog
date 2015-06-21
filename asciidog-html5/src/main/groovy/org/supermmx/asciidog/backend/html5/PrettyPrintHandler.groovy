857e6fcb46ab3545cbfe3f29d846bf40a612d97a
Xuan MA <SuperMMX@gmail.com>
2015-06-21 21:48:30 +1000

    Not write newlines for inline elements

--- asciidog-html5/src/main/groovy/org/supermmx/asciidog/backend/html5/PrettyPrintHandler.groovy
+++ asciidog-html5/src/main/groovy/org/supermmx/asciidog/backend/html5/PrettyPrintHandler.groovy
@@ -14,7 +14,30 @@ class PrettyPrintHandler implements InvocationHandler {
     private final Map<Integer, Boolean> hasChildElement = new HashMap<Integer, Boolean>();
     private static final String INDENT_CHAR = "  ";
     private static final String LINEFEED_CHAR = "\n";
- 
+
+    private static def INLINE_ELEMENTS = [
+        'a', 'abbr', 'address', 'area', 'audio',
+        'b', 'bdi', 'bdo', 'bm', 'br',
+        'cite', 'code', 'command',
+        'datalist', 'del', 'details', 'dfn',
+        'em', 'embed',
+        'font',
+        'i', 'iframe', 'img', 'input', 'ins',
+        'label', 'legend', 'link',
+        'kbd',
+        'mark', 'meter',
+        'nav',
+        'object', 'optgroup', 'option',
+        'param',
+        'q',
+        'rp', 'rt', 'ruby',
+        's', 'samp', 'script', 'select', 'small', 'source', 'span', 'strong', 'sub', 'summary', 'sup',
+        'tbody', 'td', 'time', 'track',
+        'u',
+        'var', 'video',
+        'wbr'
+    ]
+
     PrettyPrintHandler(XMLStreamWriter target) {
         this.target = target;
     }
@@ -32,11 +55,16 @@ class PrettyPrintHandler implements InvocationHandler {
             if (depth > 0) {
                 hasChildElement.put(depth - 1, true);
             }
+
             // reset state of current node
             hasChildElement.put(depth, false);
-            // indent for current depth
-            target.writeCharacters(LINEFEED_CHAR);
-            target.writeCharacters(repeat(depth, INDENT_CHAR));
+
+            if (!INLINE_ELEMENTS.contains(args[0])) {
+                // indent for current depth
+                target.writeCharacters(LINEFEED_CHAR);
+
+                target.writeCharacters(repeat(depth, INDENT_CHAR));
+            }
             depth++;
         }
         else if ("writeEndElement".equals(m)) {
@@ -47,13 +75,15 @@ class PrettyPrintHandler implements InvocationHandler {
             }
         }
         else if ("writeEmptyElement".equals(m)) {
-            // update state of parent node
-            if (depth > 0) {
-                hasChildElement.put(depth - 1, true);
+            if (!INLINE_ELEMENTS.contains(args[0])) {
+                // update state of parent node
+                if (depth > 0) {
+                    hasChildElement.put(depth - 1, true);
+                }
+                // indent for current depth
+                target.writeCharacters(LINEFEED_CHAR);
+                target.writeCharacters(repeat(depth, INDENT_CHAR));
             }
-            // indent for current depth
-            target.writeCharacters(LINEFEED_CHAR);
-            target.writeCharacters(repeat(depth, INDENT_CHAR));
         }
 
         method.invoke(target, args);
