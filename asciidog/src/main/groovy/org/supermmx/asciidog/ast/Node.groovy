4b9bc7c6ee3cdda87544406ec47dc16fd30663b4
Xuan MA <SuperMMX@gmail.com>
2015-06-23 21:42:21 +1000

    Refine isInline

--- asciidog/src/main/groovy/org/supermmx/asciidog/ast/Node.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/ast/Node.groovy
@@ -19,6 +19,7 @@ class Node {
 
         static final DOCUMENT = new Type(parent: BLOCK, name: 'document')
         static final HEADER = new Type(parent: BLOCK, name: 'header')
+        static final AUTHORS = new Type(parent: BLOCK, name: 'authors')
         static final REVISION = new Type(parent: BLOCK, name: 'revision')
         static final PREAMBLE = new Type(parent: BLOCK, name: 'preamble')
         static final SECTION = new Type(parent: BLOCK, name: 'section')
@@ -29,12 +30,13 @@ class Node {
         static final MACRO = new Type(parent: BLOCK, name: 'macro')
         static final COMMENT_LINE = new Type(parent: BLOCK, name: 'comment_line')
         
-        static final ATTRIBUTE_REFERENCE = new Type(parent: INLINE, inline: true, name: 'attribute_reference')
-        static final CROSS_REFERENCE = new Type(parent: INLINE, inline: true, name: 'xref')
-        static final FORMATTING = new Type(parent: INLINE, inline: true, name: 'formatting')
-        static final INLINE_MACRO = new Type(parent: INLINE, inline: true, name: 'inline_macro')
-        static final REPLACEMENT = new Type(parent: INLINE, inline: true, name: 'replacement')
-        static final TEXT = new Type(parent: INLINE, inline: true, name: 'text')
+        static final AUTHOR = new Type(parent: INLINE, name: 'author')
+        static final ATTRIBUTE_REFERENCE = new Type(parent: INLINE, name: 'attribute_reference')
+        static final CROSS_REFERENCE = new Type(parent: INLINE, name: 'xref')
+        static final FORMATTING = new Type(parent: INLINE, name: 'formatting')
+        static final INLINE_MACRO = new Type(parent: INLINE, name: 'inline_macro')
+        static final REPLACEMENT = new Type(parent: INLINE, name: 'replacement')
+        static final TEXT = new Type(parent: INLINE, name: 'text')
 
         static final ORDERED_LIST = new Type(parent: LIST, name: 'olist')
         static final UNORDERED_LIST = new Type(parent: LIST, name: 'ulist')
@@ -43,10 +45,6 @@ class Node {
         static final SET_ATTRIBUTE = new Type(parent: INLINE, action: true, name: 'set_attribute')
         static final SET_COUNTER = new Type(parent: INLINE, action: true, name: 'set_counter')
 
-        /**
-         * Whether the node type is for inline
-         */
-        boolean inline = false
         /**
          * Whether the node type is to do some action
          */
@@ -62,6 +60,22 @@ class Node {
 
         static boolean isCase(Type caseValue, Type switchValue) {
             return (caseValue.name == switchValue.name)
+        }
+
+        boolean isInline() {
+            boolean res = false
+
+            Type p = parent
+            while (p != null) {
+                if (p == INLINE) {
+                    res = true
+                    break
+                } else {
+                    p = p.parent
+                }
+            }
+
+            return res
         }
 
         boolean isList() {
