68154602fdf1cb57429dc9c311cf3252fee973dc
Xuan MA <SuperMMX@gmail.com>
2015-07-03 22:08:16 +1000

    Update test cases

--- asciidog-critic/src/test/groovy/org/supermmx/asciidog/critic/parser/CriticDeletionParserSpec.groovy
+++ asciidog-critic/src/test/groovy/org/supermmx/asciidog/critic/parser/CriticDeletionParserSpec.groovy
@@ -8,17 +8,17 @@ import org.supermmx.asciidog.ast.Block
 import org.supermmx.asciidog.ast.Node
 import org.supermmx.asciidog.ast.TextNode
 
-class CriticAdditionParserSpec extends AsciidogSpec {
-    def 'simple addition'() {
+class CriticDeletionParserSpec extends AsciidogSpec {
+    def 'simple deletion'() {
         given:
-        def content = '{++addition content++}'
+        def content = '{-- deletion content --}'
         def length = content.length()
         def expectedPara = builder.paragraph() {
             current.inlineNodes = [
                 new CriticNode(type: CriticNode.CRITIC_NODE_TYPE,
-                               criticType: CriticNode.CriticType.ADDITION,
+                               criticType: CriticNode.CriticType.DELETION,
                                inlineNodes: [
-                                   new TextNode('addition content')
+                                   new TextNode(' deletion content ')
                                ])
             ]
         }
