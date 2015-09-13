c22f9a86d3b44f402982958fb7d9fb8f9f19a0c6
Xuan MA <SuperMMX@gmail.com>
2015-09-13 19:00:08 +1000

    Add equals and hashcode

--- asciidog/src/main/groovy/org/supermmx/asciidog/converter/OutputChunk.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/converter/OutputChunk.groovy
@@ -3,16 +3,27 @@ package org.supermmx.asciidog.converter
 import org.supermmx.asciidog.ast.Block
 import org.supermmx.asciidog.ast.Document
 
+import groovy.transform.EqualsAndHashCode
+
+@EqualsAndHashCode(excludes=['prev', 'next'])
 class OutputChunk {
+    /**
+     * The chunk index
+     */
     int index
 
     OutputChunk prev
     OutputChunk next
 
-    String base
-    boolean chunked
+    /**
+     * The corresponding block
+     */
     Block block
 
+    /**
+     * Get the chunk name whic is used as
+     * the part of the output file
+     */
     String getName() {
         def name = null
 
@@ -23,9 +34,6 @@ class OutputChunk {
         }
 
         // attribute 'chunk-name'
-
-        // id
-
         return name
     }
 }
