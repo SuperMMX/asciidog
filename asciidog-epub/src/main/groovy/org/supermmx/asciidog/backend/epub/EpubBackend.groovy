f919717989bb7812911917f215df1a1fd4142fdc
Xuan MA <SuperMMX@gmail.com>
2015-09-24 22:31:59 +1000

    Create output epub

--- asciidog-epub/src/main/groovy/org/supermmx/asciidog/backend/epub/EpubBackend.groovy
+++ asciidog-epub/src/main/groovy/org/supermmx/asciidog/backend/epub/EpubBackend.groovy
@@ -5,12 +5,19 @@ import org.supermmx.asciidog.backend.AbstractBackend
 import org.supermmx.asciidog.converter.DocumentContext
 import org.supermmx.asciidog.ast.Document
 
+import nl.siegmann.epublib.domain.Book
+import nl.siegmann.epublib.domain.Metadata
+import nl.siegmann.epublib.domain.Resource
+import nl.siegmann.epublib.epub.EpubWriter
+
 import groovy.util.logging.Slf4j
 
 @Slf4j
 class EpubBackend extends Html5Backend {
     EpubBackend() {
         super()
+
+        parentId = id
 
         id = 'epub'
         ext = '.epub'
@@ -23,15 +30,33 @@ class EpubBackend extends Html5Backend {
 
         // set the chunk extension
         context.chunkExt = '.xhtml'
-
-        log.info "chunk ext = ${context.chunkExt}"
     }
 
     @Override
     void doEndRendering(DocumentContext context) {
+        def doc = context.document
+        def outputDir = context.outputDir
+
+        // create the epub structure
+        // Create new Book
+        Book book = new Book();
+        Metadata metadata = book.getMetadata();
+
+        // Set the title
+        metadata.addTitle(doc.header?.title);
+
         // find all the chunks
         context.chunkingStrategy.chunks.each { chunk ->
-            println "Chunk Name: ${chunk.name}"
+            def chunkFile = new File(outputDir, chunk.fileName)
+            book.addSection(chunk.block.title, new Resource(chunkFile.bytes, chunk.fileName))
         }
+
+        // create the epub
+        def base = context.attrContainer.getAttribute(Document.OUTPUT_BASE).value
+        // Create EpubWriter
+        EpubWriter epubWriter = new EpubWriter();
+
+        // Write the Book as Epub
+        epubWriter.write(book, new FileOutputStream(new File(outputDir, base + ext)));
     }
 }
