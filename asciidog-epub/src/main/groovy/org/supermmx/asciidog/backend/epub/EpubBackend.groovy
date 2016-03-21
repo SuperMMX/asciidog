5ebd4c0750f6e018dea0b9eb454eab2362d19d75
Xuan MA <SuperMMX@gmail.com>
2016-03-21 22:30:35 +1100

    Create toc for epub

--- asciidog-epub/src/main/groovy/org/supermmx/asciidog/backend/epub/EpubBackend.groovy
+++ asciidog-epub/src/main/groovy/org/supermmx/asciidog/backend/epub/EpubBackend.groovy
@@ -3,9 +3,13 @@ package org.supermmx.asciidog.backend.epub
 import org.supermmx.asciidog.backend.html5.Html5Backend
 import org.supermmx.asciidog.backend.AbstractBackend
 import org.supermmx.asciidog.converter.DocumentContext
+import org.supermmx.asciidog.ast.Block
 import org.supermmx.asciidog.ast.Document
+import org.supermmx.asciidog.ast.Node
 
 import org.supermmx.epug.creator.EpubCreator
+import org.supermmx.epug.epub.Navigation
+import org.supermmx.epug.epub.NavigationItem
 import org.supermmx.epug.epub.dcmi.DcElement
 import org.supermmx.epug.epub.dcmi.DcesTerm
 import org.supermmx.epug.epub.dcmi.DcmiTerm
@@ -63,12 +67,66 @@ class EpubBackend extends Html5Backend {
         context.chunkingStrategy.chunks.each { chunk ->
             def chunkFile = new File(outputDir, chunk.fileName)
 
-            epubCreator.addSpineItem(chunkFile.getAbsolutePath(), chunk.fileName, chunk.block.id, chunk.block.title)
+            epubCreator.addSpineItem(chunkFile.absolutePath, chunk.fileName, chunk.block.id, chunk.block.title)
         }
+
+        // add epub toc item
+        Navigation tocNav = createToc(context)
+        epubCreator.addNavigation(tocNav)
 
         // create the epub
         def base = context.attrContainer.getAttribute(Document.OUTPUT_BASE).value
 
         epubCreator.write(new File(outputDir, base + ext))
+    }
+
+    Navigation createToc(DocumentContext context) {
+        def nav = new Navigation(type: Navigation.Type.toc)
+
+        def doc = context.document
+
+        def chunk = null
+        def anchor = null
+
+        def item = nav
+        def navItems = [ ]
+
+        // go through all sections
+        def condition = { Node node ->
+            node.type == Node.Type.SECTION
+        }
+
+        def action = { Node node ->
+            Block block = (Block)node
+            if (context.chunkingStrategy.isChunkingPoint(block)) {
+                chunk = context.chunkingStrategy.getChunk(block)
+            } else {
+                anchor = Utils.normalizeId(block.id)
+            }
+
+            def childItem = new NavigationItem(title: node.title,
+                                               file: chunk?.fileName,
+                                               anchor: anchor)
+            item.items << childItem
+
+            item = childItem
+        }
+
+        def pre = { Block block ->
+            navItems << item
+        }
+
+        def post = { Block block ->
+            navItems = navItems.dropRight(1)
+            if (navItems.size() > 0) {
+                item = navItems.last()
+            } else {
+                item = null
+            }
+        }
+
+        doc.walk(condition, action, pre, post);
+
+        return nav
     }
 }
