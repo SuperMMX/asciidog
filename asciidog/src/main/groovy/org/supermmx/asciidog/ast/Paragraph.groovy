7226e68ac7f8f213902ad10b67527b2cfcf203df
Xuan MA <SuperMMX@gmail.com>
2016-04-02 21:50:30 +1100

    Accept action blocks in paragraph

--- asciidog/src/main/groovy/org/supermmx/asciidog/ast/Paragraph.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/ast/Paragraph.groovy
@@ -4,12 +4,17 @@ import groovy.transform.Canonical
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
+import groovy.util.logging.Slf4j
+
+import org.slf4j.Logger
+
 /**
  * A paragraph block that only contains inline nodes
  */
 @Canonical
 @EqualsAndHashCode(callSuper=true)
 @ToString(includeSuper=true, includePackage=false, includeNames=true)
+@Slf4j
 class Paragraph extends Block implements InlineContainer  {
     List<Inline> inlineNodes = []
 
@@ -17,8 +22,13 @@ class Paragraph extends Block implements InlineContainer  {
         type = Node.Type.PARAGRAPH
     }
 
-    InlineContainer leftShift(Inline inline) {
-        inlineNodes << inline
+    Paragraph leftShift(Node node) {
+        // only allow action block in a paragraph
+        if (node in Action) {
+            blocks << node
+        } else if (node in Inline) {
+            inlineNodes << node
+        }
 
         return this
     }
