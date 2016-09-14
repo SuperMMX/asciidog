3fed02cb1b1384c26f8d0223e2b8c92c41fedf4e
Xuan MA <SuperMMX@gmail.com>
2016-09-14 21:34:27 +1000

    Reorder node types

--- asciidog/src/main/groovy/org/supermmx/asciidog/ast/Node.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/ast/Node.groovy
@@ -21,29 +21,29 @@ class Node {
         static final NODE = new Type(name: 'node', isAbstract: true)
 
         static final BLOCK = new Type(parent: NODE, name: 'block', isAbstract: true)
-        static final ACTION= new Type(parent: BLOCK, name: 'action', isAbstract: true, isAction: true)
         static final INLINE = new Type(parent: NODE, name: 'inline', isAbstract: true)
+        static final ACTION = new Type(parent: BLOCK, name: 'action', isAbstract: true, isAction: true)
+        static final LIST = new Type(parent: BLOCK, name: 'list', isAbstract: true)
 
+        static final AUTHORS = new Type(parent: BLOCK, name: 'authors')
+        static final BLANK = new Type(parent: BLOCK, name: 'blank')
+        static final COMMENT_LINE = new Type(parent: BLOCK, name: 'comment_line')
         static final DOCUMENT = new Type(parent: BLOCK, name: 'document')
         static final HEADER = new Type(parent: BLOCK, name: 'header')
-        static final AUTHORS = new Type(parent: BLOCK, name: 'authors')
-        static final REVISION = new Type(parent: BLOCK, name: 'revision')
-        static final PREAMBLE = new Type(parent: BLOCK, name: 'preamble')
-        static final SECTION = new Type(parent: BLOCK, name: 'section')
-        static final LIST = new Type(parent: BLOCK, name: 'list')
         static final LIST_ITEM = new Type(parent: BLOCK, name: 'list_item')
-        static final PARAGRAPH = new Type(parent: BLOCK, name: 'paragraph')
-        static final TABLE = new Type(parent: BLOCK, name: 'table')
         static final MACRO = new Type(parent: BLOCK, name: 'macro')
-        static final COMMENT_LINE = new Type(parent: BLOCK, name: 'comment_line')
-        static final BLANK = new Type(parent: BLOCK, name: 'blank')
+        static final PARAGRAPH = new Type(parent: BLOCK, name: 'paragraph')
+        static final PREAMBLE = new Type(parent: BLOCK, name: 'preamble')
+        static final REVISION = new Type(parent: BLOCK, name: 'revision')
+        static final SECTION = new Type(parent: BLOCK, name: 'section')
+        static final TABLE = new Type(parent: BLOCK, name: 'table')
 
-        static final AUTHOR = new Type(parent: INLINE, name: 'author')
-        static final NULL = new Type(parent: INLINE, name: 'null')
         static final ATTRIBUTE_REFERENCE = new Type(parent: INLINE, name: 'attribute_reference')
+        static final AUTHOR = new Type(parent: INLINE, name: 'author')
         static final CROSS_REFERENCE = new Type(parent: INLINE, name: 'xref')
         static final FORMATTING = new Type(parent: INLINE, name: 'formatting')
         static final INLINE_MACRO = new Type(parent: INLINE, name: 'inline_macro')
+        static final NULL = new Type(parent: INLINE, name: 'null')
         static final REPLACEMENT = new Type(parent: INLINE, name: 'replacement')
         static final TEXT = new Type(parent: INLINE, name: 'text')
 
@@ -55,11 +55,11 @@ class Node {
         static final SET_COUNTER = new Type(parent: INLINE, isAction: true, name: 'set_counter')
 
         /**
-         * Whether the node type is abstract
+         * Whether the node with this type is abstract
          */
         boolean isAbstract = false
         /**
-         * Whether the node type is to do some action
+         * Whether the node with this type is to do some action
          */
         boolean isAction = false
         /**
@@ -125,6 +125,10 @@ class Node {
      * The parent node
      */
     Node parent
+    /**
+     * The child nodes
+     */
+    List<Node> children = []
 
     /**
      * The document that contains this node
@@ -134,5 +138,11 @@ class Node {
     Node() {
         type = Node.Type.NODE
         seq = INDEX.getAndIncrement()
+    }
+
+    Node leftShift(Node node) {
+        children << node
+
+        return this
     }
 }
