2da617b753ed19de8233b0a00b8febea5b9e57bc
Xuan MA <SuperMMX@gmail.com>
2016-09-30 22:12:49 +1000

    Add section parsing in document test cases

--- asciidog/src/test/groovy/org/supermmx/asciidog/parser/block/DocumentParserSpec.groovy
+++ asciidog/src/test/groovy/org/supermmx/asciidog/parser/block/DocumentParserSpec.groovy
@@ -1,6 +1,7 @@
 package org.supermmx.asciidog.parser.block
 
 import org.supermmx.asciidog.AsciidogSpec
+import org.supermmx.asciidog.Parser
 import org.supermmx.asciidog.ast.Node
 
 class DocumentParserSpec extends AsciidogSpec {
@@ -18,5 +19,29 @@ class DocumentParserSpec extends AsciidogSpec {
         then:
         isStart
         header.properties[(DocumentParser.HEADER_PROPERTY_DOCUMENT_TITLE)] == 'Title'
+    }
+
+    def 'document: with sections'() {
+        given:
+        def content = '''= Document Title
+
+== Section Title
+
+== Another Section
+'''
+        def eDoc = builder.document(title: 'Document Title') {
+            header {
+            }
+
+            section(level: 1, title: 'Section Title')
+            section(level: 1, title: 'Another Section')
+        }
+        def context = parserContext(content)
+
+        when:
+        def doc = Parser.parse(context)
+
+        then:
+        doc == eDoc
     }
 }
