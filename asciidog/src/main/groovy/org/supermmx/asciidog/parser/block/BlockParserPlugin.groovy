19edcdd5d6c16e8f6f6447e26ba61e0e3ec482a7
Xuan MA <SuperMMX@gmail.com>
2016-12-29 15:44:51 +1100

    Parse block attribute

--- asciidog/src/main/groovy/org/supermmx/asciidog/parser/block/BlockParserPlugin.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/parser/block/BlockParserPlugin.groovy
@@ -42,6 +42,16 @@ abstract class BlockParserPlugin extends ParserPlugin {
 )
 $
 '''
+    static final def BLOCK_ATTRIBUTES_PATTERN = ~'''(?x)
+^
+\\[                   # start with [
+(                     # 1, atrribute line
+  \\p{Blank}*
+  [\\w{},.\\#"'%].*   # '
+)
+\\]                   # end with ]
+$
+'''
 
     /**
      * Whether to skip blank lines before the block
@@ -343,6 +353,16 @@ $
                 continue
             }
 
+            // check attributes
+            def attrs = isBlockAttributes(line)
+            if (attrs != null) {
+                header.attributes.putAll(attrs)
+
+                reader.nextLine()
+
+                continue
+            }
+
             SectionParser sectionParser = PluginRegistry.instance.getPlugin(SectionParser.ID)
             if (sectionParser.checkStart(line, header, false)) {
                 header.parserId = sectionParser.id
@@ -455,6 +475,148 @@ $
         String title = m[0][1]
 
         return title
+    }
+
+    /**
+     * Whether the line represents block attributes definition, like
+     *
+     * [style, key="value" new-key='new value' ]
+     */
+    protected static Map<String, String> isBlockAttributes(String line) {
+        if (line == null) {
+            return null
+        }
+
+        def m = BLOCK_ATTRIBUTES_PATTERN.matcher(line)
+        if (!m.matches()) {
+            return null
+        }
+
+        line = m[0][1]
+
+        return parseAttributes(line)
+    }
+
+    /**
+     * Parse general attributes, not the document attributes.
+     *
+     * @param text the attributes text with []
+     */
+    protected static Map<String, String> parseAttributes(String text) {
+        def line = text
+
+        def attrs = [:] as LinkedHashMap<String, String>
+
+        // size of the attribute line
+        def size = line.length()
+
+        def key = null
+        def value = null
+
+        def index = 0
+
+        // main loop
+        while (index < size) {
+            def buf = []
+
+            def quote = null
+
+            def ch = line[index]
+
+            // skip blanks
+            while (ch == ' ') {
+                index ++
+                ch = line[index]
+            }
+
+            // starting quote
+            if (ch == "'" || ch == '"') {
+                quote = ch
+                index ++
+
+                ch = ''
+            }
+
+            // find the key or value in quotes or not
+            while (index < size) {
+                ch = line[index]
+
+                if (quote == null) {
+                    if (',='.indexOf(ch) >= 0) {
+                        // not in quotes, and is a delimiter
+                        break
+                    }
+                }
+
+                // ending quote
+                if ((ch == "'" || ch == '"')
+                    && ch == quote) {
+                    index ++
+                    ch = ''
+                    break
+                }
+
+                buf << ch
+                ch = ''
+
+                index ++
+            }
+
+            // join the characters
+            def str = buf.join('')
+
+            // trim the value if not in quote
+            if (quote == null) {
+                str = str.trim()
+            } else {
+                // skip all blanks after the quote
+                while (index < size) {
+                    ch = line[index]
+                    if (ch == ' ') {
+                        index ++
+                    } else {
+                        break
+                    }
+                }
+
+                quote = null
+            }
+
+            if (index >= size) {
+                ch = ','
+            } else {
+                ch = line[index]
+            }
+
+            if (ch == ',') {
+                // end of the value
+                // an attribute defintion is over
+
+                if (key == null) {
+                    key = str
+                } else {
+                    value = str
+                }
+
+                // add the attribute
+                attrs[(key)] = value
+
+                // reset
+                key = null
+                value = null
+            } else if (ch == '=') {
+                // end of the key
+                key = str
+            } else {
+                // invalid
+            }
+
+            if (index < size) {
+                index ++
+            }
+        }
+
+        return attrs
     }
 
     static enum ParserInfoType {
