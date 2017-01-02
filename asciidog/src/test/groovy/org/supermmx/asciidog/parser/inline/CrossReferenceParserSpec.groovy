2a39e719957754aa1311d43088f1dd9c57b1a3d6
Xuan MA <SuperMMX@gmail.com>
2017-01-03 07:52:12 +1100

    Move to parser/inline

--- asciidog/src/test/groovy/org/supermmx/asciidog/parser/inline/CrossReferenceParserSpec.groovy
+++ asciidog/src/test/groovy/org/supermmx/asciidog/parser/inline/CrossReferenceParserSpec.groovy
@@ -1,21 +1,26 @@
-package org.supermmx.asciidog
+package org.supermmx.asciidog.parser.inline
 
+import org.supermmx.asciidog.AsciidogSpec
 import org.supermmx.asciidog.ast.Block
 import org.supermmx.asciidog.ast.Node
 import org.supermmx.asciidog.ast.Paragraph
+import org.supermmx.asciidog.parser.block.ParagraphParser
 
-class CrossReferenceSpec extends AsciidogSpec {
+class CrossReferenceParserSpec extends AsciidogSpec {
+    def parser = new ParagraphParser()
+
     def 'xref using angled bracket syntax'() {
         given:
         def content = '<<tigers>>'
-        def length = content.length()
+        def context = parserContext(content)
+        context.parserId = parser.id
+
         def expectedPara = builder.para {
             xref 'tigers'
         }
 
-
         when:
-        def para = parser(content).parseParagraph(new Block())
+        def para = parser.parse(context)
 
         then:
         para == expectedPara
@@ -24,14 +29,15 @@ class CrossReferenceSpec extends AsciidogSpec {
     def 'xref with undercore and space'() {
         given:
         def content = '<<_cross ref>>'
-        def length = content.length()
+        def context = parserContext(content)
+        context.parserId = parser.id
+
         def expectedPara = builder.para {
             xref '_cross ref'
         }
 
-
         when:
-        def para = parser(content).parseParagraph(new Block())
+        def para = parser.parse(context)
 
         then:
         para == expectedPara
@@ -40,14 +46,16 @@ class CrossReferenceSpec extends AsciidogSpec {
     def 'xref start with colon'() {
         given:
         def content = '<<:title>>'
-        def length = content.length()
+        def context = parserContext(content)
+        context.parserId = parser.id
+
         def expectedPara = builder.para {
             xref ':title'
         }
 
 
         when:
-        def para = parser(content).parseParagraph(new Block())
+        def para = parser.parse(context)
 
         then:
         para == expectedPara
