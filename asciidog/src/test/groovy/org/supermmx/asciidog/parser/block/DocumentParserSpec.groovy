e4f5887e984b53d8af4de7ebd916827b60163bc8
Xuan MA <SuperMMX@gmail.com>
2018-03-13 22:31:28 +1100

    Add tests case of createBlock for document parser

--- asciidog/src/test/groovy/org/supermmx/asciidog/parser/block/DocumentParserSpec.groovy
+++ asciidog/src/test/groovy/org/supermmx/asciidog/parser/block/DocumentParserSpec.groovy
@@ -2,10 +2,67 @@ package org.supermmx.asciidog.parser.block
 
 import org.supermmx.asciidog.AsciidogSpec
 import org.supermmx.asciidog.Parser
+import org.supermmx.asciidog.ast.Document
 import org.supermmx.asciidog.ast.Node
 
 class DocumentParserSpec extends AsciidogSpec {
     def parser = new DocumentParser()
+
+    def 'createBlock: article'() {
+        given:
+
+        def content = '= Document Title'
+        def edoc = builder.document(title: 'Document Title') {
+        }
+
+        def context = parserContext(content)
+        def header = new BlockParserPlugin.BlockHeader()
+
+        when:
+        def doc = parser.createBlock(context, null, header)
+
+        then:
+        edoc == doc
+        context.attributes.getAttribute(Document.DOCTYPE).value ==  Document.DocType.article.toString()
+    }
+
+    def 'createBlock: inline as section'() {
+        given:
+
+        def content = '== Section Title'
+        def edoc = builder.document {
+        }
+
+        def context = parserContext(content)
+        def header = new BlockParserPlugin.BlockHeader()
+
+        when:
+        def doc = parser.createBlock(context, null, header)
+
+        then:
+        edoc == doc
+        context.attributes.getAttribute(Document.DOCTYPE).value ==  Document.DocType.inline.toString()
+        context.lexer.peek().value == '=='
+    }
+
+    def 'createBlock: inline as other'() {
+        given:
+
+        def content = 'this is a paragraph'
+        def edoc = builder.document {
+        }
+
+        def context = parserContext(content)
+        def header = new BlockParserPlugin.BlockHeader()
+
+        when:
+        def doc = parser.createBlock(context, null, header)
+
+        then:
+        edoc == doc
+        context.attributes.getAttribute(Document.DOCTYPE).value ==  Document.DocType.inline.toString()
+        context.lexer.peek().value == 'this'
+    }
 
     def 'checkStart: good'() {
         given:
