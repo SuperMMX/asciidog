9110aba308116f2a0376fc9f00087225940fae74
Xuan MA <SuperMMX@gmail.com>
2018-04-18 23:27:03 +1000

    Parse attribute reference

--- asciidog/src/main/groovy/org/supermmx/asciidog/parser/inline/AttributeReferenceParser.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/parser/inline/AttributeReferenceParser.groovy
@@ -1,16 +1,22 @@
 package org.supermmx.asciidog.parser.inline
 
+import static org.supermmx.asciidog.parser.TokenMatcher.*;
+
 import org.supermmx.asciidog.Parser
 import org.supermmx.asciidog.ast.Inline
 import org.supermmx.asciidog.ast.Node
+import org.supermmx.asciidog.ast.InlineContainer
 import org.supermmx.asciidog.ast.InlineInfo
 import org.supermmx.asciidog.ast.AttributeReferenceNode
+import org.supermmx.asciidog.parser.ParserContext
+import org.supermmx.asciidog.parser.action.AttributeEntryParser
+import org.supermmx.asciidog.parser.inline.InlineContext
 import org.supermmx.asciidog.parser.inline.InlineParserPlugin
 
 /*
-import org.supermmx.asciidog.ast.AttributeSettingNode
-import org.supermmx.asciidog.ast.CounterSettingNode
-*/
+ import org.supermmx.asciidog.ast.AttributeSettingNode
+ import org.supermmx.asciidog.ast.CounterSettingNode
+ */
 
 import java.util.regex.Matcher
 
@@ -39,11 +45,61 @@ class AttributeReferenceParser extends InlineParserPlugin {
 
     static final String ID = 'plugin:parser:inline:attribute_reference'
 
+    static final String INLINE_PROPERTY_ATTRIBUTE_NAME = 'attrName'
+
+    // TODO: set, counter and counter2
+    static final CHECK_MATCHER = sequence([
+        literal('{'),
+        AttributeEntryParser.ATTRIBUTE_NAME_MATCHER,
+        literal('}')
+    ])
+
+    static final Closure CHECK_ACTION = { String name, ParserContext context, Map<String, Object> props, boolean matched ->
+        if (!matched) {
+            return
+        }
+
+        if (name == 'name') {
+            context.inlineContext[INLINE_PROPERTY_ATTRIBUTE_NAME] = context.lexer.joinTokensFromMark()
+        }
+    }
+
     AttributeReferenceParser() {
         id = ID
         nodeType = Node.Type.ATTRIBUTE_REFERENCE
 
         pattern = ATTRIBUTE_REFERENCE_PATTERN
+    }
+
+    @Override
+    protected boolean doCheckStart(ParserContext context) {
+        if (context.lexer.peek().value != '{') {
+            return false
+        }
+
+        context.inlineContext = new InlineContext()
+
+        return CHECK_MATCHER.matches(context, false, [:], CHECK_ACTION)
+    }
+
+    @Override
+    protected boolean doCheckEnd(ParserContext context) {
+        context.inlineContext = null
+
+        return true
+    }
+
+    @Override
+    protected Inline doParse(ParserContext context, InlineContainer parent) {
+        // TODO: handle set, counter and counter2
+
+        // check header attributes
+        def inlineContext = context.inlineContext
+        def attrName = inlineContext[INLINE_PROPERTY_ATTRIBUTE_NAME]
+
+        def inline = new AttributeReferenceNode(name: attrName)
+
+        return inline
     }
 
     @Override
@@ -54,7 +110,7 @@ class AttributeReferenceParser extends InlineParserPlugin {
         if (action == 'set') {
             //inline = new AttributeSettingNode()
         } else if (action == 'counter'
-            || action == 'counter2') {
+                   || action == 'counter2') {
             //inline = new CounterSettingNode()
         } else {
             // normal reference
@@ -77,4 +133,3 @@ class AttributeReferenceParser extends InlineParserPlugin {
         return true
     }
 }
-
