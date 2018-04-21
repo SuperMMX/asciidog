7ac74198e092a5a35f3027844aa44df45b2519e1
Xuan MA <SuperMMX@gmail.com>
2018-04-21 18:32:40 +1000

    Refactor critic markup parsing

--- asciidog-critic/src/main/groovy/org/supermmx/asciidog/critic/CriticNode.groovy
+++ asciidog-critic/src/main/groovy/org/supermmx/asciidog/critic/CriticNode.groovy
@@ -10,15 +10,33 @@ import groovy.transform.TupleConstructor
 @EqualsAndHashCode(callSuper=true)
 @TupleConstructor
 class CriticNode extends Inline implements InlineContainer {
+    static final String TAG_ADDITION = '++'
+    static final String TAG_DELETION = '--'
+    static final String TAG_SUBSTITUTION = '~~'
+    static final String TAG_COMMENT_START = '>>'
+    static final String TAG_COMMENT_END = '<<'
+    static final String TAG_HIGHLIGHT = '=='
+
     static final Node.Type CRITIC_NODE_TYPE =
         new Node.Type(parent: Node.Type.INLINE, name: 'critic')
 
     static enum CriticType {
-        ADDITION,
-        DELETION,
-        COMMENT,
-        HIGHLIGHT,
-        SUBSTITUTION
+        ADDITION(TAG_ADDITION),
+        DELETION(TAG_DELETION),
+        COMMENT(TAG_COMMENT_START, TAG_COMMENT_END),
+        HIGHLIGHT(TAG_HIGHLIGHT),
+        SUBSTITUTION(TAG_SUBSTITUTION)
+
+        String startTag
+        String endTag
+
+        CriticType(String startTag, String endTag = null) {
+            this.startTag = startTag
+            this.endTag = endTag
+            if (this.endTag == null) {
+                this.endTag = this.startTag
+            }
+        }
     }
 
     CriticType criticType
--- asciidog-critic/src/main/groovy/org/supermmx/asciidog/critic/parser/CriticAdditionParser.groovy
+++ asciidog-critic/src/main/groovy/org/supermmx/asciidog/critic/parser/CriticAdditionParser.groovy
@@ -1,17 +1,63 @@
 package org.supermmx.asciidog.critic.parser
 
+import static org.supermmx.asciidog.parser.TokenMatcher.*
+
 import org.supermmx.asciidog.critic.CriticNode
 
+import org.supermmx.asciidog.ast.InlineContainer
+import org.supermmx.asciidog.parser.ParserContext
+import org.supermmx.asciidog.parser.TokenMatcher
+
+import groovy.util.logging.Slf4j
+
+import org.slf4j.Logger
+
+@Slf4j
+@Slf4j(value='userLog', category="AsciiDog")
 class CriticAdditionParser extends AbstractCriticParser {
     static final String ID = 'plugin:parser:inline:critic:addition'
+
+    static final TokenMatcher START_MATCHER_FOR_SUBSTITUTION = sequence([
+        literal('~'),
+        literal('>')
+    ])
+
+    static final TokenMatcher END_MATCHER_FOR_SUBSTITUTION = sequence([
+        literal(CriticNode.TAG_SUBSTITUTION),
+        literal('}')
+    ], true)
 
     CriticAdditionParser() {
         id = ID
         criticType = CriticNode.CriticType.ADDITION
-        pattern = ~"""(?Usxm)
-\\{\\+\\+
-(.+)    # 1, content
-\\+\\+\\}
-"""
     }
+
+    @Override
+    protected boolean doCheckStart(ParserContext context, InlineContainer parent) {
+        def result = false
+
+        if (parent in CriticNode
+            && parent.criticType == CriticNode.CriticType.SUBSTITUTION) {
+            result = START_MATCHER_FOR_SUBSTITUTION.matches(context)
+        } else {
+            result = super.doCheckStart(context, parent)
+        }
+
+        return result
+    }
+
+    @Override
+    protected boolean doCheckEnd(ParserContext context, InlineContainer parent) {
+        def result = false
+
+        if (parent in CriticNode
+            && parent.criticType == CriticNode.CriticType.SUBSTITUTION) {
+            result = END_MATCHER_FOR_SUBSTITUTION.matches(context)
+        } else {
+            result = super.doCheckEnd(context, parent)
+        }
+
+        return result
+    }
+
 }
