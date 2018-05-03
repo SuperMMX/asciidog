d4c1813705fb05684558ab13d8eaea44e8071423
Xuan MA <SuperMMX@gmail.com>
2018-05-03 23:15:34 +1000

    Add comments

--- asciidog/src/main/groovy/org/supermmx/asciidog/ast/Macro.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/ast/Macro.groovy
@@ -2,6 +2,9 @@ package org.supermmx.asciidog.ast
 
 import org.supermmx.asciidog.Subtype
 
+/**
+ * Macro trait for both Block Macro and Inline Macro
+ */
 trait Macro implements Subtype {
     String name
     String target
