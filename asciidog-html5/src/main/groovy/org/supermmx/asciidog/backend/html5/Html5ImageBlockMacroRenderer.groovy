bcd5f6507024a10046a4d3dd5a1c6ce840cfcb9e
Xuan MA <SuperMMX@gmail.com>
2018-05-03 23:08:34 +1000

    Use the same director as the path prefix for include

--- asciidog-html5/src/main/groovy/org/supermmx/asciidog/backend/html5/Html5ImageBlockMacroRenderer.groovy
+++ asciidog-html5/src/main/groovy/org/supermmx/asciidog/backend/html5/Html5ImageBlockMacroRenderer.groovy
@@ -5,9 +5,18 @@ import org.supermmx.asciidog.ast.BlockMacro
 import org.supermmx.asciidog.backend.AbstractBlockMacroRenderer
 import org.supermmx.asciidog.converter.DocumentContext
 
+import java.nio.file.Files
+import java.nio.file.Paths
+
+import groovy.util.logging.Slf4j
+
+import org.slf4j.Logger
+
 /**
  * The renderer for image block macro
  */
+@Slf4j
+@Slf4j(value='userLog', category="AsciiDog")
 class Html5ImageBlockMacroRenderer extends AbstractBlockMacroRenderer {
     Html5ImageBlockMacroRenderer() {
         name = 'image'
@@ -19,17 +28,29 @@ class Html5ImageBlockMacroRenderer extends AbstractBlockMacroRenderer {
 
         context.writer.writeStartElement('img')
 
-        def path = ''
+        def path = macroNode.target
 
         // consider attribute "imagesdir"
         def imagesdir = context.attrContainer['imagesdir']
         if (imagesdir != null) {
-            path = "${imagesdir}/"
+            path = "${imagesdir}/${macroNode.target}"
         }
 
         // TODO: consider absolute path and URI
-        context.writer.writeAttribute('src', "${path}${macroNode.target}")
+        context.writer.writeAttribute('src', path)
 
         context.writer.writeEndElement()
+
+        // copy the image to the destination, can't do this after rendering
+        // because the AST itself may not have the correct imagesdir value
+        def inputDir = context.document.attrs.inputFile?.parentFile
+        if (inputDir != null) {
+            def imageFile = new File(context.outputDir, path)
+            if (!imageFile.parentFile.exists()) {
+                imageFile.parentFile.mkdirs()
+            }
+
+            Files.copy(inputDir.toPath().resolve(path), imageFile.toPath())
+        }
     }
 }
