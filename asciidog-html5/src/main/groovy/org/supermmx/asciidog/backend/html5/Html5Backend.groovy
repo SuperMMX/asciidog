71884ed9ff9f43e6f746b18b4869a96a35999043
Xuan MA <SuperMMX@gmail.com>
2018-05-09 23:16:33 +1000

    Delete unused imports

--- asciidog-html5/src/main/groovy/org/supermmx/asciidog/backend/html5/Html5Backend.groovy
+++ asciidog-html5/src/main/groovy/org/supermmx/asciidog/backend/html5/Html5Backend.groovy
@@ -1,32 +1,10 @@
 package org.supermmx.asciidog.backend.html5
 
-import org.supermmx.asciidog.backend.Backend
-import org.supermmx.asciidog.backend.AbstractBackend
 import org.supermmx.asciidog.backend.AbstractTemplateBackend
-import org.supermmx.asciidog.backend.NullNodeRenderer
 import org.supermmx.asciidog.backend.TemplateManager
-import org.supermmx.asciidog.ast.AdocList
-import org.supermmx.asciidog.ast.Author
-import org.supermmx.asciidog.ast.Block
-import org.supermmx.asciidog.ast.Document
-import org.supermmx.asciidog.ast.Header
-import org.supermmx.asciidog.ast.Node
-import org.supermmx.asciidog.ast.Section
-import org.supermmx.asciidog.ast.Paragraph
-import org.supermmx.asciidog.ast.Preamble
-import org.supermmx.asciidog.ast.InlineContainer
-import org.supermmx.asciidog.ast.Inline
-import org.supermmx.asciidog.ast.TextNode
-import org.supermmx.asciidog.ast.FormattingNode
-import org.supermmx.asciidog.ast.CrossReferenceNode
-import org.supermmx.asciidog.ast.ListItem
 import org.supermmx.asciidog.converter.DocumentContext
 
 import groovy.util.logging.Slf4j
-
-import javax.xml.stream.XMLOutputFactory
-import javax.xml.stream.XMLStreamWriter
-import java.lang.reflect.Proxy
 
 @Slf4j
 class Html5Backend extends AbstractTemplateBackend {
