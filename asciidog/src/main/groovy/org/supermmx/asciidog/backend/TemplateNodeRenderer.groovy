8a91d963cf402986567afb565001af8365c9cb43
Xuan MA <SuperMMX@gmail.com>
2021-07-10 23:12:55 +1000

    Render block wrapping element and title

--- asciidog/src/main/groovy/org/supermmx/asciidog/backend/TemplateNodeRenderer.groovy
+++ asciidog/src/main/groovy/org/supermmx/asciidog/backend/TemplateNodeRenderer.groovy
@@ -1,6 +1,8 @@
 package org.supermmx.asciidog.backend
 
+import org.supermmx.asciidog.ast.Block
 import org.supermmx.asciidog.ast.Node
+import org.supermmx.asciidog.ast.Section
 import org.supermmx.asciidog.converter.DocumentContext
 
 import groovy.util.logging.Slf4j
@@ -10,6 +12,8 @@ import groovy.util.logging.Slf4j
  */
 @Slf4j
 class TemplateNodeRenderer extends AbstractLeafNodeRenderer {
+    private static final TEMPLATE_BLOCK_NODE = new Block()
+
     @Override
     boolean accept(Node node) {
         // accept any nodes
@@ -18,6 +22,16 @@ class TemplateNodeRenderer extends AbstractLeafNodeRenderer {
 
     @Override
     void doPre(DocumentContext context, Node node) {
+        // only for content blocks
+        if (toRenderHeader(node)) {
+            // the wrapping block element, id, and other necessary attributes
+            context.backend.renderNode(context, TEMPLATE_BLOCK_NODE, node, 'header')
+
+            // render the block title
+            context.backend.renderNode(context, TEMPLATE_BLOCK_NODE, node, 'title')
+        }
+
+        // the real node element
         context.backend.renderNode(context, node, node, 'pre')
     }
 
@@ -29,5 +43,16 @@ class TemplateNodeRenderer extends AbstractLeafNodeRenderer {
     @Override
     void doPost(DocumentContext context, Node node) {
         context.backend.renderNode(context, node, node, 'post')
+
+        // only for content blocks
+        if (toRenderHeader(node)) {
+            // the block element, id, and other necessary attributes
+            context.backend.renderNode(context, TEMPLATE_BLOCK_NODE, node, 'header_post')
+        }
+
+    }
+
+    private boolean toRenderHeader(Node node) {
+        return node.type.isType(Node.Type.BLOCK) && !node.type.isType(Node.Type.LIST_ITEM)
     }
 }
